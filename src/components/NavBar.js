951001cd400942f5a71e7a70da88341aa711ed85
PapitoSpence <PapitoSpence>
2023-01-24 11:50:42 +0300

    completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,14 +3,13 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return (
-  <nav>
-    {/* display an <a> tag for each link here */}
-    {links.map((link, index) => {
-      return <a href={link} key = {index}>{link.toUpperCase()}</a>
-    })}
-  </nav>
-  )
+  return <nav>{/* display an <a> tag for each link here */ 
+  <>
+    <a href="#home">home</a>
+    <a href="#about">about</a>
+    <a href="#projects">projects</a>
+  </>
+  }</nav>;
 }
 
 export default NavBar;
--- src/components/PrjectList.js
+++ src/components/PrjectList.js
@@ -2,24 +2,13 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  const projectItems = projects.map((item) => {
-    return (
-      
-        <ProjectItem 
-        key={item.id} 
-        name={item.name} 
-        about={item.about} 
-        technologies={item.technologies}
-      />
-
-    );
-  });
-  
- // console.log(projects);
+  console.log(projects);
+  const projectContainer = projects.map((project) => {
+    return <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies}/>})
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{projectItems}</div>
+      <div id="project-list">{projectContainer/* render ProjectItem components here */}</div>
     </div>
   );
 }
